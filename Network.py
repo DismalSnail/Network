dbd0365374f2c88c36aa1577866d846bc6a3ff67
DismalSnail <1031053585@qq.com>
2019-04-08 15:31:08 +0800

    网络建立完成

--- Network.py
+++ Network.py
@@ -1,4 +1,27 @@
 import networkx as nx
+import salience as sa
+import csv
 
-G=nx.Graph()
-G.add_weighted_edges_from([1,2,0.1])
+csvFile = open("CSV/trainweight.csv", "r")
+csvreader = csv.reader(csvFile)
+
+edgeList = []
+
+for item in csvreader:
+    middlelist = []
+    if csvreader.line_num == 1:
+        continue
+    else:
+        middlelist.append(int(item[0]))
+        middlelist.append(int(item[1]))
+        middlelist.append(int(item[2]))
+        edgeList.append(tuple(middlelist))
+
+for item in edgeList:
+    print(item)
+
+G = nx.Graph()
+G.add_weighted_edges_from(edgeList)
+print(G.number_of_edges())
+
+csvFile.close()
--- salience.py
+++ salience.py
@@ -56,18 +56,18 @@ def _SPT(G, r, weight='weight'):
     N = G.order()
     T = np.zeros((N,N))
     
-    #Add the 'proximity' weight to each link (1/w) - WARNING: w has to be not zero!
+    # Add the 'proximity' weight to each link (1/w) - WARNING: w has to be not zero!
     for i,j in G.edges():
         w = G[i][j][weight]
         G[i][j]['proximity'] = 1./w
     
     paths=nx.shortest_path(G,source=r, weight='proximity')
-    #each path is a dictionary with key: r and value:the list of nodes in the path
+    # each path is a dictionary with key: r and value:the list of nodes in the path
     
-    #Filling T based on the presence of a link in at least one of the shortest paths
+    # Filling T based on the presence of a link in at least one of the shortest paths
     for k, path in paths.iteritems(): 
         for i in range(len(path)-1):
-            T[path[i]][path[i+1]]=1 #updating the matrix T because there is a link
+            T[path[i]][path[i+1]]=1 # updating the matrix T because there is a link
     return T
 
 def salience(G,weight='weight'):
